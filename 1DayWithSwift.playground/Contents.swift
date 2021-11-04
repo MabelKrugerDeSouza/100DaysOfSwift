0515c8c219764eb8c88a14aee92759d9ce1652d0
Mabel <mabelksouza@gmail.com>
2021-11-04 16:22:46 -0300

    First Day: multiLines,Booleans and doubles, string interpolation, contraindo, type annotations

--- 1DayWithSwift.playground/Contents.swift
+++ 1DayWithSwift.playground/Contents.swift
@@ -1,4 +1,32 @@
 import UIKit
 
-var helloWord = "Hello, playground"
-var byeWolrd = "Good bye"
+var helloWord = "Hello, playground" // string
+var byeWolrd = "Good bye" // string
+var age = 18 // integer
+
+var mult_lines = """
+primeira linha = first line \
+segunda linha = second line \
+terceira linha = third line
+"""
+
+var booleans = true
+var double = 3.141
+
+//string interpolation
+var score = 34
+var showTheScore = "your score was \(score)"
+var results = "The test results are:  \(showTheScore)"
+
+
+//Contraints
+let name = "Douglas"
+
+//Type annotations
+var helloWorld = "Hello World"
+var name2: String = "Mabel"
+var age2: Int = 18
+var height: Double = 1.59
+var trueOrFalse: Bool = true
+
+
