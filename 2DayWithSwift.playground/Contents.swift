d2e237bd931200d593d303faab1db07afb5f18eb
Mabel <mabelksouza@gmail.com>
2021-11-04 16:58:33 -0300

    start the second day

---
+++ 2DayWithSwift.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//Arrays
+
+let capitaoAmerica = "Capitao America"
+let homemAranha = "Homem Aranha"
+let viuvaNegra = "Viuva Negra"
+let homemDeFerro = "Homem de Ferro"
+
+//                      0               1           2           3
+var Vingadores = [capitaoAmerica, homemAranha, viuvaNegra, homemAranha]
+Vingadores[2]
+Vingadores.append("Homem Formiga") // add a new element
+
+
+// is diferent of array, because they have no order to follow and dont repeat the word.
+let color = Set(["blue","red","green"])
+let color2 = Set(["blue", "blue", "green", "pink"])
+
+
