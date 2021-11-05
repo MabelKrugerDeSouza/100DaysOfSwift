79d29fce64311364feca794a05b16cb4dc05d855
Mabel <mabelksouza@gmail.com>
2021-11-05 09:45:59 -0300

    Dictionaries, Dictionaries with defaults, creating empty collection, enumerations, enums associates Values and enum raw values

--- 2DayWithSwift.playground/Contents.swift
+++ 2DayWithSwift.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreGraphics
 
 //Arrays
 
@@ -28,5 +29,72 @@ names.1
 //Arrays vs set vs tumple
 
 let tumble = (number: "1", number2: "2", number3: "3")
-let set = 
+let set = Set(["Enrique", "Gustavo", "Enrique"])
+let array = ["Enrique", "Jose", "Amanda", "Mabel"]
     
+//Dictionaries
+//[String: Double]
+//[String: String]
+//[Int : String]
+//[String: Bool]
+
+let lastName = [
+    "Gustavo": "Henrique",
+    "Mabel": "Kruger",
+    "Isadora": "Hogler"
+]
+
+//dictionaries
+//Default
+
+var iceCream = ["Mabel" : "Ovomaltine", "Mabelk": "Goiaba"]
+
+iceCream["Mabel"]
+iceCream["Jose", default: "Nao se sabe"]
+
+
+//creating empty collection
+
+var teams = [String:String]()
+teams["Mabel"] = "Pink"
+
+var results = [Int]()
+var words = Set<String>()
+var int = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var exemple = Array<String>()
+
+// Enumerations
+
+enum Result{
+    case sucess
+    case failure
+}
+
+var sucess = Result.sucess
+var failure = Result.failure
+
+//Enuns associated Values
+
+enum Activity {
+    case bored
+    case runnig(destinacion: String)
+    case talking(topic: String)
+    case singing(Volume: Int)
+}
+
+let talkingSome = Activity.talking(topic: "Anime")
+
+
+//Enum raw values
+
+enum Planet: Int{
+    case terra
+    case marte
+    case outroSla
+}
+
+let planeta = Planet(rawValue: 0)
+
+
